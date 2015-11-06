44a9b4014146b73ab4cc49a044fc79287347ceb5
cwp <cwp@work>
2015-11-06 10:15:02 +0800

    recompiled

--- dist/alpaca/bootstrap/alpaca.js
+++ dist/alpaca/bootstrap/alpaca.js
@@ -22663,6 +22663,10 @@ this["HandlebarsPrecompiled"]["bootstrap-edit"]["message"] = Handlebars.template
 
 })(jQuery);
 
+/**
+ * Uses the spectrum plugin to provide a color picker.
+ * This used to rely on HTML5 but no longer.
+ */
 (function($) {
 
     var Alpaca = $.alpaca;
@@ -22677,10 +22681,44 @@ this["HandlebarsPrecompiled"]["bootstrap-edit"]["message"] = Handlebars.template
          */
         setup: function()
         {
+            var self = this;
+
+            this.spectrumAvailable = false;
+            if (!self.isDisplayOnly() && typeof($.fn.spectrum) !== "undefined")
+            {
+                this.spectrumAvailable = true;
+            }
+
             // default html5 input type = "color";
-            this.inputType = "color";
+            if (typeof(this.options.spectrum) === "undefined" && self.spectrumAvailable)
+            {
+                this.inputType = "color";
+            }
 
             this.base();
+
+            // set up default spectrum settings
+            if (typeof(this.options.spectrum) === "undefined")
+            {
+                this.options.spectrum = {};
+            }
+            if (typeof(this.options.spectrum.showInput) === "undefined")
+            {
+                this.options.spectrum.showInput = true;
+            }
+            if (typeof(this.options.spectrum.showPalette) === "undefined")
+            {
+                this.options.spectrum.showPalette = true;
+            }
+            if (typeof(this.options.spectrum.preferredFormat) === "undefined")
+            {
+                this.options.spectrum.preferredFormat = "hex3";
+            }
+            if (typeof(this.options.spectrum.clickoutFiresChange) === "undefined")
+            {
+                this.options.spectrum.clickoutFiresChange = true;
+            }
+            this.options.spectrum.color = this.data;
         },
 
         /**
@@ -22695,6 +22733,28 @@ this["HandlebarsPrecompiled"]["bootstrap-edit"]["message"] = Handlebars.template
          */
         getType: function() {
             return "string";
+        },
+
+        afterRenderControl: function(model, callback)
+        {
+            var self = this;
+
+            this.base(model, function() {
+
+                // if we can render the spectrum plugin...
+                if (self.spectrumAvailable && self.control)
+                {
+                    setTimeout(function() {
+                        $((self.control)[0]).spectrum(self.options.spectrum);
+                    }, 100);
+
+                    $(self.control).on('change.spectrum', function(e, tinycolor) {
+                        self.setValue(tinycolor.toHexString());
+                    });
+                }
+
+                callback();
+            });
         },
 
         /* builder_helpers */
